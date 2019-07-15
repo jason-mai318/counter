4b5765a79f55804334ed404de2a8c5176fe3eab1
Jason <jasonmai318@gmail.com>
2019-07-14 23:20:14 -0400

    restart

--- src/App.js
+++ src/App.js
@@ -11,28 +11,17 @@ class Counter extends Component {
 
 
 increment = () => {
-  if(this.state.counter < 20) {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-  if(this.state.double){
-    if(this.state.counter < 19){
-      this.setState({
-        counter: this.state.counter + 2
-      })
-    }
-  }
+  this.setState({
+    counter: this.state.counter +1
+  })
 };
 
   render() {
-    console.log("This is line 13 representating state", this.state.counter);
-    console.log("This is the double value", this.state.double);
     return(
       <div className="container">
-        <div className="navbar">Counter.js</div>
+        <div className="navbar">Counter</div>
         <div className="counter">
-          <h1>{this.state.counter}</h1>
+          {this.state.counter}
           <button type="button" onClick={this.increment}>Increment</button>
           </div>
       </div>
