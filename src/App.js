c98fb0975f81a65abaefe9f65de04e3f1952e8e9
Jason <jasonmai318@gmail.com>
2019-07-15 00:09:11 -0400

    added limits

--- src/App.js
+++ src/App.js
@@ -1,62 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       counter: 0,
       size: 1,
+      min_limit: 0,
+      max_limit: 25,
     }
   }
 
 
-increment = () => {
-  this.setState({
-    counter: this.state.counter + this.state.size
-  })
-};
+  increment = () => {
+    if (this.state.counter + this.state.size <= this.state.max_limit) {
+      this.setState({
+        counter: this.state.counter + this.state.size
+      })
+    }
+    else {
+      this.setState({
+        counter: this.state.counter
+      })
+    }
+  };
 
-decrement = () => {
-  this.setState({
-    counter: this.state.counter - this.state.size
-  })
-};
+  decrement = () => {
+    if (this.state.counter - this.state.size >= this.state.min_limit) {
+      this.setState({
+        counter: this.state.counter - this.state.size
+      })
+    }
+    else {
+      this.setState({
+        counter: this.state.counter
+      });
+    }
+  };
 
-toggle = () => {
-  if (this.state.size == 1 ) {
+  toggle = () => {
+    if (this.state.size == 1) {
+      this.setState({
+        size: 2
+      })
+    }
+    if (this.state.size == 2) {
+      this.setState({
+        size: 1
+      })
+    }
+  };
+
+
+  reset = () => {
     this.setState({
-      size: 2
+      counter: 0
     })
-  }
-  if (this.state.size == 2 ) {
-    this.setState({
-      size: 1
-    })
-  }
-};
-
-
-reset = () => {
-  this.setState({
-    counter: 0
-  })
-};
+  };
 
   render() {
-    return(
+    return (
       <div className="container">
         <div className="navbar">Counter</div>
         <div className="counter">
           <h1>{this.state.counter}</h1>
-          <h4>Current Increment Size: {this.state.size}</h4>
+          <h4>Minimum: {this.state.min_limit} | Maximum: {this.state.max_limit} | Current Increment Size: {this.state.size}</h4>
           <button type="button" onClick={this.increment}>Add</button>
           <button type="button" onClick={this.decrement}>Subtract</button>
           <button type="button" onClick={this.toggle}>Toggle Increment Size</button>
           <button type="button" onClick={this.reset}>Reset</button>
-          </div>
+        </div>
       </div>
     )
   }
+
 }
 
 export default Counter;
