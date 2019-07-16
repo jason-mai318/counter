412a5a5a2303cabeee30e273fcc65e2cc9e2889d
Student <jsitbon@qcc.cuny.edu>
2019-07-16 13:04:45 -0400

    Updates

---
+++ precache-manifest.88d3d5e4f39e4a932a3bdea1f97341b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c037e4d725953a3f996a5eebd26c483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac437d3d159f1e82f024",
+    "url": "/static/css/main.606c3544.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ac437d3d159f1e82f024",
+    "url": "/static/js/main.b3284f67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
